a018c0b69683bc2cb3ab129871d2b3cc663be03c
indreshkumartd <59084945+indreshkumartd@users.noreply.github.com>
2021-08-25 11:01:00 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "hello I am here"
+pwd
+cd $td_wf_project_name
+pwd
 digdag check
 digdag push $td_wf_project_name -e $td_wf_endpoint -X client.http.headers.authorization="TD1 $td_master_apikey"
